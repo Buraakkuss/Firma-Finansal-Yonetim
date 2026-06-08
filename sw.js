e18eaf89b4b56f7d8bbaefa4f08fef4bc19d6a11
Buraakkuss <t.burakkus@gmail.com>
2026-06-09 02:02:39 +0300

    TL EUR USD dovizli islem sistemi v1.0.27
    TL EUR USD dovizli islem sistemi v1.0.27

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'firma-finansal-v1-0-25';
+const CACHE_NAME = 'firma-finansal-v1-0-27';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {

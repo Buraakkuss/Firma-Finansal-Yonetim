26ae4221d968c75dfed7d1ee5adfa7cd1c219907
Buraakkuss <t.burakkus@gmail.com>
2026-06-10 01:06:29 +0300

    v1.0.44
    v1.0.44

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'firma-finansal-v1-0-43';
+const CACHE_NAME = 'firma-finansal-v1-0-44';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
